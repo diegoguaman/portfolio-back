76060d7a1516d93e651203aa19eec768bf872df1
Diego Guaman <diegoguamanmedina@gmail.com>
2025-06-25 11:01:10 +0200

    add prefix api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['http://localhost:5173' /* tu dominio de prod */],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const config = new DocumentBuilder()
     .setTitle('Portfolio API')
